4c295b421c2f800617cb5eb680ae046041af90a4
Gunin Khanna <guninkhanna@Gunins-MacBook-Pro.local>
2026-03-06 22:52:26 +1100

    hiding all default ST options

--- app/app.py
+++ app/app.py
@@ -4,10 +4,29 @@ st.set_page_config(layout="centered")
 
 hide_streamlit_style = """
 <style>
-[data-testid="stSidebar"] {display: none;}
-[data-testid="collapsedControl"] {display: none;}
+
+/* Hide hamburger menu */
+#MainMenu {visibility: hidden;}
+
+/* Hide footer */
+footer {visibility: hidden;}
+
+/* Hide header */
+header {visibility: hidden;}
+
+/* Hide top-right toolbar */
+[data-testid="stToolbar"] {display: none;}
+
+/* Hide Streamlit deploy button */
+[data-testid="stDecoration"] {display: none;}
+
+/* Hide bottom-right manage app button */
+[data-testid="stStatusWidget"] {display: none;}
+
 </style>
 """
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
