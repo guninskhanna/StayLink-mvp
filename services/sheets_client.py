4fe5cddb7ea67d55bc74c521a4972cbf94a17ae7
Gunin Khanna <guninkhanna@Gunins-MacBook-Pro.local>
2026-03-07 17:40:56 +1100

    fixing sheets and aws client

--- services/sheets_client.py
+++ services/sheets_client.py
@@ -8,13 +8,13 @@ from googleapiclient.http import MediaIoBaseUpload
 import io
 import boto3
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
-    region_name=st.secrets["AWS_REGION"]
-)
-
+def get_s3_client():
+    return boto3.client(
+        "s3",
+        aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
+        region_name=st.secrets["AWS_REGION"]
+    )
 BUCKET = st.secrets["AWS_BUCKET"]
 
 scope = [
@@ -92,11 +92,14 @@ def generate_transaction_id():
 
 def upload_receipt(file, filename):
 
+    s3 = get_s3_client()
+
+    bucket = st.secrets["AWS_BUCKET"]
     key = f"receipts/{filename}"
 
     s3.upload_fileobj(
         file,
-        BUCKET,
+        bucket,
         key,
         ExtraArgs={
             "ContentType": file.type
